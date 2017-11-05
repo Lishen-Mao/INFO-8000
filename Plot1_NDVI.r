424a2e9b471ab85d3e0252969ee58824bfb0328b
Lishen-Mao <lmao@emich.edu>
2017-11-05 18:49:27 -0500

    It is hard to tell any difference in the NDVI of these two pixels.

--- Plot1_NDVI.r
+++ Plot1_NDVI.r
@@ -16,7 +16,23 @@ BSSWNDVI<-subset(MarshSR, pixel == "SW" & site == "BS")
 #Filter the data in my MarshSR dataset where pixel equals 'SE' and site equals 'BS', and copy these data to a new variable
 BSSENDVI<-subset(MarshSR, pixel == "SE" & site == "BS")
 
-##put date on the x-axis and NDVI on the y-axis:
+##put date on the x-axis and NDVI on the y-axis, line graph
+#From this plot, it is hard to tell which pixel has more consistent and higher NDVI value
 ggplot(data=BSSENDVI) + 
-  geom_line(mapping = aes(x = date, y = NDVI, group = 1)) +
-  labs(x = "2015 to 2016", y = "NDVI", title = "NDVI value in SW and SE pixels of Bayou Sauvage National wildlife refuge")
+  geom_line(mapping = aes(x = date, y = NDVI, group = 1), color = rgb(0,0,0, alpha = 0.2))+
+  geom_line(data = BSSWNDVI, mapping = aes(x = date, y = NDVI, group = 1), color = rgb(0,0,1, alpha = 0.3)) +
+  labs(x = "2015 to 2016", y = "NDVI", title = "SW and SE pixel NDVI value in Bayou Sauvage National wildlife refuge")
+
+#mean NDVI 
+meanNDVI <- mean ((BSSENDVI$NDVI+BSSWNDVI$NDVI)/2)
+
+###put date on the x-axis and NDVI on the y-axis, point graph
+ggplot(data=BSSENDVI) + 
+  geom_point(mapping = aes(x = date, y = NDVI, group = 1), color = rgb(0,0,0, alpha = 0.2))+
+  geom_point(data = BSSWNDVI, mapping = aes(x = date, y = NDVI, group = 1), color = rgb(0,0,1, alpha = 0.3)) +
+  labs(x = "2015 to 2016", y = "NDVI", title = "SW and SE pixel NDVI value in Bayou Sauvage National wildlife refuge")+
+  geom_hline(yintercept = meanNDVI)
+
+
+
+abline(h=meanNDVI)
